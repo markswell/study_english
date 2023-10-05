deffd2f9636d98a4dedfb5dd0a6d93f57cfd318e
markswell menezes <markswellmenezes@outlook.com>
2023-10-05 11:23:29 -0300

    Adds swagger documentation

--- study-english_backend/src/main/java/com/markswell/studyenglish/resource/MediaResource.java
+++ study-english_backend/src/main/java/com/markswell/studyenglish/resource/MediaResource.java
@@ -1,6 +1,10 @@
 package com.markswell.studyenglish.resource;
 
 import com.markswell.studyenglish.service.MediaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,27 +15,48 @@ import static org.springframework.http.MediaType.APPLICATION_PDF_VALUE;
 @RestController
 @RequestMapping("/media")
 @RequiredArgsConstructor
+@Tag(name = "Resource", description = "It solves media's data")
 public class MediaResource {
 
     private final MediaService mediaService;
 
     @CrossOrigin("*")
     @GetMapping(value = "/{book}", produces = APPLICATION_PDF_VALUE)
-    public ResponseEntity<byte[]> getBookPdf(@PathVariable("book") String book) {
+    @Operation(
+            summary = "Book",
+            description = "It solves PDF's book file"
+    )
+    public ResponseEntity<byte[]> getBookPdf(
+            @Parameter(description = "It is the book name for searching")
+            @PathVariable("book") String book) {
         return ResponseEntity.ok(mediaService.getPdf(book));
     }
 
     @CrossOrigin("*")
     @GetMapping(value = "/{bookId}/{lessonId}/{audioId}", produces = APPLICATION_OCTET_STREAM_VALUE)
-    public ResponseEntity<byte[]> getAudio(@PathVariable("bookId") Long bookId,
-                                           @PathVariable("lessonId") Long lessonId,
-                                           @PathVariable("audioId") Long audioId) {
+    @Operation(
+            summary = "Audio",
+            description = "It solves audio's lesson file"
+    )
+    public ResponseEntity<byte[]> getAudio(
+            @Parameter(description = "It is the book id for searching")
+            @PathVariable("bookId") Long bookId,
+            @Parameter(description = "It is the lesson id for searching")
+            @PathVariable("lessonId") Long lessonId,
+            @Parameter(description = "It is the audio id for searching")
+            @PathVariable("audioId") Long audioId) {
         return ResponseEntity.ok(mediaService.getAudio(bookId, lessonId, audioId));
     }
 
     @CrossOrigin("*")
     @GetMapping(value = "/class/{classId}", produces = APPLICATION_OCTET_STREAM_VALUE)
-    public ResponseEntity<byte[]> getVideo(@PathVariable("classId") Long classId) {
+    @Operation(
+            summary = "Video",
+            description = "It solves video's class file"
+    )
+    public ResponseEntity<byte[]> getVideo(
+            @Parameter(description = "It is the class id for searching")
+            @PathVariable("classId") Long classId) {
         return ResponseEntity.ok(mediaService.getVideo(classId));
     }
 
--- study-english_backend/src/main/java/com/markswell/studyenglish/resource/PathResource.java
+++ study-english_backend/src/main/java/com/markswell/studyenglish/resource/PathResource.java
@@ -1,5 +1,7 @@
 package com.markswell.studyenglish.resource;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import com.markswell.studyenglish.dto.BookDto;
 import org.springframework.http.ResponseEntity;
@@ -18,18 +20,25 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @RequestMapping("/path")
 @RequiredArgsConstructor
+@Tag(name = "Path", description = "It solves paths to medias of course")
 public class PathResource {
 
     private final PathService pathService;
 
     @CrossOrigin("*")
     @GetMapping(value = "/book", produces = APPLICATION_JSON_VALUE)
+    @Operation(
+            summary = "Book paths",
+            description = "It solves paths URL to access audio lessons and PDF's books")
     public ResponseEntity<List<BookDto>> getBooks() throws IOException {
         return ResponseEntity.ok(pathService.getBooks());
     }
 
     @CrossOrigin("*")
     @GetMapping(value = "/video", produces = APPLICATION_JSON_VALUE)
+    @Operation(
+            summary = "Video paths",
+            description = "It solves paths URL to access video classes")
     public ResponseEntity<List<VideoDto>> getVideos() {
         return ResponseEntity.ok(pathService.getVideos());
     }
